3f3a6e6a0ac07556bb51da79f1bc705b422d5cfb
RachanaS <RachanaS@CL-F8JLMN3>
2024-09-02 11:41:39 +0530

    Add project files.

---
+++ MyApp.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp;
+
+namespace MyApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void TestAdd()
+        {
+            var calc = new Calculator();
+            var res = calc.Add(3, 1);
+            Assert.That(res, Is.EqualTo(4));
+        }
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyApp;
+
+Console.WriteLine("Hello, World!");
+
+Calculator calc = new Calculator();
+Console.WriteLine("Addition of 5 and 3: " + calc.Add(5, 3));
+
+
+//namespace MyApp
+//{
+//    public class Program
+//    {
+//        public void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
